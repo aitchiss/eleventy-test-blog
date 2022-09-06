7471b745ed58e86d3ddd38a9314318e0d47eeb0c
Suzanne Aitchison <suzanne.aitchison@netlify.com>
2022-09-06 15:06:13 +0100

    add hello world function

---
+++ netlify/functions/hello.js
@@ -0,0 +1,7 @@
+exports.handler = async function (event, context) {
+    console.log("szlhkg sdhlgjhsd hjksdlhg dshjlgkhs gjhkddgjkhgs fkjhgjkadshd dskjhdg sdjkhg dsjkhx gjksdhz ghdszkdhg sdhg kjsdhxg jdshg jdgjkhgds")
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Hello World" }),
+    };
+  };
