83ea6a9fda9809b862957a98e6b09ca25d7883dd
Suzanne Aitchison <suzanne.aitchison@netlify.com>
2022-09-06 15:12:37 +0100

    again

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,3 +1,6 @@
 exports.handler = async function (event, context) {
-    console.log("szlhkg sdhlgjhsd hjksdlhg dshjlgkhs gjhkddgjkhgs fkjhgjkadshd dskjhdg sdjkhg dsjkhx gjksdhz ghdszkdhg sdhg kjsdhxg jdshg jdgjkhgds");
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ message: "szlhkg sdhlgjhsd hjksdlhg dshjlgkhs gjhkddgjkhgs fkjhgjkadshd dskjhdg sdjkhg dsjkhx gjksdhz ghdszkdhg sdhg kjsdhxg jdshg jdgjkhgds" }),
+      };
   };
