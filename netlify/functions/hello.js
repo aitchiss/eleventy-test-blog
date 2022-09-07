e01fa546a6dd3c6b3ee512ee86bdbd33ba338198
Suzanne Aitchison <suzanne.aitchison@netlify.com>
2022-09-07 13:30:19 +0100

    smol change

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,6 +1,6 @@
 
 export function handler(event, context, callback) {
-    console.log('Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit. Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit. Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.')
+    console.log('Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit. Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit. Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit!!')
     callback(null, {
       statusCode: 200,
       body: JSON.stringify({ msg: 'Hello, World!' }),
