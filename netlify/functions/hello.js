8972feca71ae8287ca45dd20668d869066cd8700
Suzanne Aitchison <suzanne.aitchison@netlify.com>
2022-09-07 10:46:50 +0100

    long log line

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,6 +1,6 @@
 
 export function handler(event, context, callback) {
-    console.log('queryStringParameters', event.queryStringParameters)
+    console.log('Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit. Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit. Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.')
     callback(null, {
       statusCode: 200,
       body: JSON.stringify({ msg: 'Hello, World!' }),
